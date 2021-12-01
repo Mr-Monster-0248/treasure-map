2dc047bc28d76438832dc4388b51832237bef2d3
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-12-01 11:25:13 +0100

    fix: change file name utilisation

--- src/services/getFormatedLinesFromFile.ts
+++ src/services/getFormatedLinesFromFile.ts
@@ -1,11 +1,10 @@
 import * as fs from 'fs';
-import path = require('path');
 
 export default function getFormatedLinesFromFile(
   filepath: string
 ): Promise<string[]> {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, filepath), (err, data) => {
+    fs.readFile(filepath, (err, data) => {
       if (err) reject(err);
       else {
         const lines = data
